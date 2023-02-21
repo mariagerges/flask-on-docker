b440df223c9db7bef2ab9f8add7862d74b2ede14
Maria Gerges <mgerges23@cmc.edu>
2023-02-20 21:08:19 -0800

    last

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,8 +1,15 @@
 import os
 
-from flask import (Flask, jsonify, send_from_directory, request,)
+from flask import (
+        Flask, 
+        jsonify, 
+        send_from_directory, 
+        request,
+)
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
+
+
 app = Flask(__name__)
 app.config.from_object("project.config.Config")
 db = SQLAlchemy(app)
